971ebdbad3fba5c3fdd12536a3dc7607556c6db5
KobelMar <max.marcisiak@gmail.com>
2022-12-14 16:25:33 +0100

    done.

--- src/store/index.js
+++ src/store/index.js
@@ -1,25 +1,44 @@
 import { createStore } from "redux";
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = { counter: 0, counterVisible: true };
+
+createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter += action.amount;
+    },
+    toggleCounter(state) {
+      state.counterVisible = !state.counterVisible;
+    },
+  },
+});
 
 const counterReducer = (state = initialState, action) => {
   if (action.type === "increment") {
     return {
-        ...state,
+      ...state,
       counter: state.counter + 1,
     };
   }
   if (action.type === "decrement") {
     return {
       counter: state.counter - 1,
-      counterVisible: state.counterVisible
+      counterVisible: state.counterVisible,
     };
   }
   if (action.type === "increase") {
     return {
       counter: state.counter + action.amount,
-      counterVisible: state.counterVisible
-
+      counterVisible: state.counterVisible,
     };
   }
   if (action.type === "toggle") {
